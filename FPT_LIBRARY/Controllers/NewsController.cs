95f686e1f75ce205b9886f75933268cc8f5576bb
Hiệp Phạm Văn <kaissken@outlook.com>
2022-12-17 19:38:15 +0700

    Commit

---
+++ FPT_LIBRARY/Controllers/NewsController.cs
@@ -0,0 +1,24 @@
+﻿using FPT_LIBRARY.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FPT_LIBRARY.Controllers
+{
+    public class NewsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();   
+        // GET: News
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Partial_News_Home()
+        {
+            var items=db.News.Take(3).ToList();
+            return PartialView(items);
+        }
+    }
+}
