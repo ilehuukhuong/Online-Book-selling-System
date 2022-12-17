4fae295cdf0c745ae6a7c7f5c295666b117c9d43
Hữu Khương Lê <ilehuukhuong@gmail.com>
2022-12-17 19:51:40 +0700

    xoa

--- FPT_LIBRARY/Migrations/Configuration.cs
+++ FPT_LIBRARY/Migrations/Configuration.cs
@@ -5,6 +5,7 @@
     using System.Data.Entity.Migrations;
     using System.Linq;
     using FPT_LIBRARY.Models;
+    using FPT_LIBRARY.Models.EF;
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
 
@@ -29,6 +30,20 @@
                 var adminLastName = "user";
                 string adminRole = "Administrator";
                 CreateAdminUser(context, adminEmail, adminPhoneNumber, adminUserName, adminPassword, adminFirstName, adminLastName, adminRole);
+            }
+            if (!context.ProductCategories.Any())
+            {
+                CreateBookCategory(context, "Action and Adventure", "/Uploads/images/genre/2.jpg");
+                CreateBookCategory(context, "Classics", "/Uploads/images/genre/1.jpg");
+                CreateBookCategory(context, "Fantasy", "/Uploads/images/genre/3.jpg");
+                context.SaveChanges();
+            }
+            if (!context.Categories.Any())
+            {
+                CreateCategory(context, "Home", 1);
+                CreateCategory(context, "Product", 2);
+                CreateCategory(context, "Contact", 3);
+                context.SaveChanges();
             }
         }
         private void CreateAdminUser(ApplicationDbContext context, string adminEmail, string adminPhoneNumber, string adminUserName, string adminPassword, string adminFirstName, string adminLastName, string adminRole)
@@ -72,6 +87,45 @@
                 throw new Exception(string.Join("; ", addAdminRoleResult.Errors));
             }
         }
+
+        private void CreateBookCategory(ApplicationDbContext context,string title, string icon)
+        {
+            var cate = new ProductCategory();
+            cate.Title = title;
+            cate.Icon = icon;
+            cate.CreateDate = DateTime.Now;
+            cate.ModifiedDate= DateTime.Now;
+            cate.Description = title;
+            cate.SeoTitle = title;
+            cate.Alias = FPT_LIBRARY.Models.Common.Filter.FilterChar(title);
+            context.ProductCategories.Add(cate);
+        }
+
+        private void CreateCategory(ApplicationDbContext context, string title, int position)
+        {
+            var cate = new Category();
+            cate.Title = title;
+            cate.IsActive = true;
+            cate.Position = position;
+            cate.CreateDate = DateTime.Now;
+            cate.ModifiedDate = DateTime.Now;
+            cate.Description = title;
+            cate.SeoTitle = title;
+            cate.Alias = FPT_LIBRARY.Models.Common.Filter.FilterChar(title);
+            context.Categories.Add(cate);
+        }
+
+        //private void CreateBook(ApplicationDbContext db,
+        //    string title, string body, DateTime date, string authorUsername)
+        //{
+        //    var book = new Product();
+        //    book.Title = title;
+        //    book.Body = body;
+        //    book.Date = date;
+        //    book.Author = context.Users.Where(u => u.UserName == authorUsername).FirstOrDefault();
+        //    context.Posts.Add(post);
+        //}
+
         //  This method will be called after migrating to the latest version.
 
         //  You can use the DbSet<T>.AddOrUpdate() helper extension method
